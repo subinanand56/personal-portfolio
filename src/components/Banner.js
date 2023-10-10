c95f29d91ebb4165f5dc77834bab04600688f3c7
Subin Anand <subinanand8078@gmail.com>
2023-10-10 15:11:12 +0530

    ss

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import headerImg from "../assets/img/header-img.png";
 import { useState, useEffect } from "react";
 import'animate.css';
 import TrackVisibility from "react-on-screen";
-import resume from '../assets/img/s.pdf';
+import resume from '../assets/img/ss.pdf';
 
 
 export const Banner = () => {
